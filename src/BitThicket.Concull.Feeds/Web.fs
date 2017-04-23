52b0fee69fb75e2b0f5917404e6df37ae1e2e394
Ben Collins <aggieben@gmail.com>
2017-04-22 19:37:52 -0500

    get a simple working fetchContent with unit test

--- src/BitThicket.Concull.Feeds/Web.fs
+++ src/BitThicket.Concull.Feeds/Web.fs
@@ -1,6 +1,12 @@
 namespace BitThicket.Concull.Feeds
 
 module Web =
+    open System.Net.Http
 
-    let fetchContent client uri =
-        null
+    let fetchContent (client:HttpClient) (uri:string) =
+        async {
+            let! resp = Async.AwaitTask (client.GetAsync(uri))
+
+            return! Async.AwaitTask (resp.Content.ReadAsStringAsync())
+        }
+        
--- test/BitThicket.Concull.Feeds.Test/WebTests.fs
+++ test/BitThicket.Concull.Feeds.Test/WebTests.fs
@@ -1,13 +1,30 @@
 module WebTests
 
 open System
+open System.IO
 open System.Net.Http
+open System.Threading.Tasks
 open Xunit
 open BitThicket.Concull.Feeds
 
 [<Fact>]
 let ``extract content from http response`` () =
-    use client = new HttpClient()
-    let result = Web.fetchContent client "http://benjamincollins.com/blog/a-working-example-of-permissions-authorization/"
-    
-    Assert.NotNull(result)
+    Console.WriteLine(Directory.GetCurrentDirectory())
+
+    let handler = { new HttpMessageHandler() with
+                        override x.SendAsync(request, cancellationToken) =
+                            Task.FromResult(new HttpResponseMessage(
+                                                Content = new StreamContent(File.OpenRead("html/a-working-example-of-permissions-authorization.html"))
+                                            )
+                            )
+                    }
+    let client = new HttpClient(handler);
+
+    async {
+        let! result = Web.fetchContent client "http://benjamincollins.com/blog/a-working-example-of-permissions-authorization/"
+
+        Assert.NotNull(result)
+        Console.WriteLine(result)
+    }
+
+
